529e3b519a8d4541990133b61a35a4409b2da3a0
Kyle Martin <kymartin4505@gmail.com>
2022-02-07 13:28:32 -0800

    search flight by flight number

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from flask import Flask, flash, redirect, render_template, request
 from dotenv import dotenv_values
-from flask import Flask
 import requests
 import json
+import os
 
 app = Flask(__name__)
 config = dotenv_values('.env')
@@ -11,15 +12,31 @@ AVIATION_STACK_API_KEY = config.get('AVIATION_STACK_API_KEY')
 @app.route('/')
 def getFlightData():
     url = 'http://api.aviationstack.com/v1/flights'
-    params = {'access_key': AVIATION_STACK_API_KEY}    
+    search_flight_num = request.args.get('get-flight-by-id')
     
-    req = requests.get(url, params=params)
-    res = json.loads(req.text)
-    data = json.dumps(res, indent=4)
+    if search_flight_num == '':
+        flash('Please search for a flight using the flight number')
+        return redirect('/', code=302)  # successful redirect 
+    
+    params = {
+        'access_key': AVIATION_STACK_API_KEY,
+        'flight_number': search_flight_num
+    }
+     
+    # req = requests.get(url, params=params) 
+    # res = json.loads(req.text)
+    # data = json.dumps(res)
+    
+    # with open('flightData.json', 'w') as outfile:
+    #     outfile.write(data)
+    return render_template('index.html')
 
-    with open('flightData.json', 'w') as outfile:
-        outfile.write(data)
+app.route('/my-flight', methods=['GET', 'POST'])
+def getFlightById():
+    return render_template('myFlight.html')
+    
 
 if __name__ == '__main__':
+    app.secret_key = os.urandom(24)
     app.run(debug=True, port=8000)
 
