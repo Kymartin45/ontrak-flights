b35226b92c6be59d2c31f8cd228de38c72a570b7
Kyle Martin <kymartin4505@gmail.com>
2022-02-08 12:04:34 -0800

    req scheduled/cancelled flights

--- main.py
+++ main.py
@@ -10,31 +10,41 @@ config = dotenv_values('.env')
 AVIATION_STACK_API_KEY = config.get('AVIATION_STACK_API_KEY')
 
 @app.route('/')
-def getFlightData():
-    url = 'http://api.aviationstack.com/v1/flights'
-    search_flight_num = request.args.get('get-flight-by-id')
-    
-    if search_flight_num == '':
-        flash('Please search for a flight using the flight number')
-        return redirect('/', code=302)  # successful redirect 
-    
-    params = {
-        'access_key': AVIATION_STACK_API_KEY,
-        'flight_number': search_flight_num
-    }
-     
+def flightHomePage():
     # req = requests.get(url, params=params) 
     # res = json.loads(req.text)
-    # data = json.dumps(res)
+    # data = json.dumps(res, indent=4)
     
     # with open('flightData.json', 'w') as outfile:
     #     outfile.write(data)
     return render_template('index.html')
 
-app.route('/my-flight', methods=['GET', 'POST'])
-def getFlightById():
-    return render_template('myFlight.html')
+@app.route('/flight', methods=['GET', 'POST'])
+def getFlightByNum():
+    url = 'http://api.aviationstack.com/v1/flights'
+    search_flight_num = request.args.get('flight-number')
+    params = {
+        'access_key': AVIATION_STACK_API_KEY,
+        'flight_number': search_flight_num
+    }
+    f = open('flightData.json') # temp read json file to avoid unecessary api req's
+    data = json.load(f)
+    airlines = data['data']
     
+    airline_data = []
+    for airline in airlines:
+        airline_data.append({
+            'flight_date': airline['flight_date'],
+            'flight_status': airline['flight_status'],
+            'airline_name': airline['airline']['name'],
+            'flight_number': airline['flight']['number']
+        })
+        
+    if search_flight_num == '':
+        flash('Please search for a flight using the flight number')
+        return redirect('/', code=302)
+    
+    return render_template('myFlight.html', airline_data = airline_data)
 
 if __name__ == '__main__':
     app.secret_key = os.urandom(24)
