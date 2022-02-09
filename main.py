625f68ad4209462fd040706388a5ebda9e57de69
Kyle Martin <kymartin4505@gmail.com>
2022-02-09 15:09:18 -0800

    departure and arrival locations added

--- main.py
+++ main.py
@@ -23,14 +23,14 @@ def flightHomePage():
 @app.route('/flight', methods=['GET', 'POST'])
 def getFlightByNum():
     now = datetime.now()
-    curr_date = now.strftime('%Y-%m-%d') # WIP 
+    curr_date = now.strftime('%Y-%m-%d') 
     
     url = 'http://api.aviationstack.com/v1/flights'
     search_flight_num = request.args.get('flight-number')
     params = {
         'access_key': AVIATION_STACK_API_KEY,
         'flight_number': search_flight_num,
-        'flight_date': curr_date
+        'flight_date': curr_date    # not needed in long run when searching personal flight num (param CAN be used if user wants historical flight records)
     }
     f = open('flightData.json') # temp read json file to avoid unecessary api req's
     data = json.load(f)
@@ -42,14 +42,16 @@ def getFlightByNum():
             'flight_date': airline['flight_date'],
             'flight_status': airline['flight_status'],
             'airline_name': airline['airline']['name'],
+            'departure_airport': airline['departure']['airport'],
+            'arrival_airport': airline['arrival']['airport'],
             'flight_number': airline['flight']['number'],
             'active_flight': airline['live'],               # if live: return lat, long else null 
-            'flight_latitude': airline['live']['latitude'],   # flights MUST be active/en-route in order to get lat, long
-            'flight_longitude': airline['live']['longitude']
+            # 'flight_latitude': airline['live']['latitude'],   # flights MUST be active/en-route in order to get lat, long
+            # 'flight_longitude': airline['live']['longitude']
         })
         
     if search_flight_num == '':
-        flash('Please search for a flight using the flight number')
+        flash('Please search for a flight using the flight number', 'error')
         return redirect('/', code=302)
     
     return render_template('myFlight.html', airline_data = airline_data, date = curr_date)
